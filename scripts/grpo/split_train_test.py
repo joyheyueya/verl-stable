9696629688cf75085c668b9ff90cd614deb86b7a
Joy He-Yueya <heyueya@next7.stanford.edu>
2025-05-16 01:48:43 -0700

    change path

--- scripts/grpo/split_train_test.py
+++ scripts/grpo/split_train_test.py
@@ -1,5 +1,6 @@
 import datasets
-ds = datasets.load_dataset('joyheyueya/0512_ilcr_train_final_pair100k_rl_single')
+ds = datasets.load_dataset('joyheyueya/0512_ilcr_train_final_pair100k_rl_single', split='train')
 ds = ds.train_test_split(0.1, seed=42)
+
 ds['train'].to_parquet('/home/ubuntu/rl/train.parquet')
 ds['test'].to_parquet('/home/ubuntu/rl/test.parquet')
